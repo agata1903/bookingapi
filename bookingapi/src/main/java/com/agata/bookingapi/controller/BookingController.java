8227114e65bfc66a790756e6709539843c55474c
agata <agata.fernanda4@gmail.com>
2026-03-27 09:42:46 -0300

    Adiciona o bookingRepository como atributo com seu construtor para que ele seja acessado pela classe e seus métodos sejam utilizados

--- bookingapi/src/main/java/com/agata/bookingapi/controller/BookingController.java
+++ bookingapi/src/main/java/com/agata/bookingapi/controller/BookingController.java
@@ -6,4 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/bookings")
 public class BookingController {
+
+    private final BookingRepository bookingRepository;
+
+    public BookingController(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
 }
