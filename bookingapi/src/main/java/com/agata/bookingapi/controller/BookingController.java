eb16829107abf239552e121e201026e3e2e75e7b
agata <agata.fernanda4@gmail.com>
2026-03-30 09:52:31 -0300

    fix: Mudar os nomes dos métodos para melhor transparência de cada um

--- bookingapi/src/main/java/com/agata/bookingapi/controller/BookingController.java
+++ bookingapi/src/main/java/com/agata/bookingapi/controller/BookingController.java
@@ -14,8 +14,28 @@ public class BookingController {
     }
 
     @PostMapping
-    public Booking scheduleTime(@Valid @RequestBody Booking booking) {
-        if (bookingRepository.existsAppointmentTime(booking.getAppointmentTime())) {
+    public Booking createBooking(@Valid @RequestBody BookingDTO request) {
+        Booking booking = new Booking();
+        booking.setClientName(request.getClientName());
+        booking.setClientPhone(request.getClientPhone());
+        booking.setAppointmentTime(request.getAppointmentTime());
+        booking.setService(request.getService());
+        booking.setStatus(BookingStatus.SCHEDULED);
+        booking.setProfessionalName(request.getProfessionalName());
+
+        return bookingRepository.save(booking);
+    }
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingRepository.findAll();
+    }
+
+    @PatchMapping("/{id}")
+    public Booking updateBooking(@Valid @RequestBody Booking booking) {
+        Booking existsBooking = bookingRepository.findById(booking.getId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Booking not found!"));
+        if (!existsBooking.getAppointmentTime().equals(booking.getAppointmentTime()) && bookingRepository.existsAppointmentTime(booking.getAppointmentTime())) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Time already reserved!");
         }
         return bookingRepository.save(booking);
